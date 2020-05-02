a88305be8898e05b9bae38936a41c8eba1b97407
HarithaRugada <haritharugada@gmail.com>
2020-05-02 15:39:12 +0530

    added usecase4

--- wincoin.sh
+++ wincoin.sh
@@ -1,8 +1,8 @@
-#! /bin/bash/ -x
+#! /bin/bash/
 echo "Program for the win of head or tail"
 hwin=0
 twin=0
-i=0
+i=1
 while [ $hwin -lt 21 ] && [ $twin -lt 21 ]
 do
 	random=$RANDOM
@@ -15,9 +15,26 @@ do
 	fi
 	i=$(( $i+1 ))
 done
+if [ $hwin -eq $twin ]
+then
+	while [ $(( $hwin-$twin )) -eq 2 ]
+	do
+		i=$(( $i+1 ))
+		flip[$i]=$(( $RANDOM%2 ))
+		if [ ${flip[$i]} -eq 1 ]
+	        then
+        	        hwin=$(( $hwin+1 ))
+        	else
+                	twin=$(( $twin+1 ))
+        	fi
+	done
+fi
 if [ $hwin -gt $twin ]
 then
-        echo "Heads has won 21 times"
+	echo "Head win"
 else
-        echo "Tails has won 21 times"
+	echo "Tail win"
 fi
+echo "Heads win > " $hwin
+echo "Tails win > "$twin
+echo "Total turns > "$(( $i-1 ))
